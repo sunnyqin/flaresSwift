2eeeca7f01f5ca5a8bd68a261eb42d7fd6839e3d
sunnyqin <ganso1989@163.com>
2015-02-28 14:06:31 +0800

    add login code with swift

--- FlaresSwift/User.swift
+++ FlaresSwift/User.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+class User {
+	
+	var userID: String?
+	var userToken: String?
+	var countryCode: String?
+	var phoneNumber: String?
+	var deviceId: String?
+	var phoneNumberVerified: Int?
+	var email: String?
+	
+	init() {
+
+	}
+	
+	class func createUserFromJSON(json: JSON) -> User {
+		var user = User()
+		
+		if let userid = json["user_id"].string {
+			user.userID = userid
+		}
+		if let token = json["token"].string {
+			user.userToken = token
+		}
+		if let phone = json["phone_number"].string {
+			user.phoneNumber = phone
+		}
+		if let countryCode = json["country_code"].string {
+			user.countryCode = countryCode
+		}
+		if let deviceId = json["device_id"].string {
+			user.deviceId = deviceId
+		}
+		if let email = json["email"].string {
+			user.email = email
+		}
+		if let phoneNumberVerified = json["phone_number_verified"].int {
+			user.phoneNumberVerified = phoneNumberVerified
+		}
+		return user
+	}
+}
