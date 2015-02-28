9fcfdf14ac3908e848b8290d15b9c316fadcd410
sunnyqin <ganso1989@163.com>
2015-02-28 09:23:47 +0800

    git test code

--- FlaresSwift/FLSignUpViewController.swift
+++ FlaresSwift/FLSignUpViewController.swift
@@ -17,6 +17,32 @@ class FLSignUpViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
+		self.navigationController?.navigationBarHidden = true
+		
+		let background = UIImageView(frame: self.view.bounds)
+		background.image = UIImage(named: "bg_blur")
+		self.view.addSubview(background)
+		
+		let titleLabel = UILabel(frame: CGRectMake(50, 0, self.view.bounds.size.width - 50 * 2, 56))
+		titleLabel.textColor = UIColor.whiteColor()
+		titleLabel.backgroundColor = UIColor.clearColor()
+		titleLabel.font = UIFont.systemFontOfSize(20.0)
+		titleLabel.textAlignment = NSTextAlignment.Center
+		titleLabel.text = "Sign-up"
+		self.view.addSubview(titleLabel)
+		
+		let backButton = UIButton()
+		backButton.frame = CGRectMake(0, 0, 60, 60)
+		backButton.setImage(UIImage(named: "btn_back"), forState: UIControlState.Normal)
+		backButton.addTarget(self, action: "back", forControlEvents: UIControlEvents.TouchUpInside)
+		self.view.addSubview(backButton)
+	}
+	
+	func back() -> Void {
+		self.dismissViewControllerAnimated(false, completion: {})
+	}
+	
+	func signUp() -> Void {
 		
 	}
 }
